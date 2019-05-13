d22e09d02ffa1b94d5bbaa3a8c6af8d62ab17a39
Chen Yu Pao <windperson@gmail.com>
2019-05-13 14:13:40 +0800

    Add some default argument value

--- hello.ps1
+++ hello.ps1
@@ -1,19 +1,21 @@
 #!/usr/bin/env pwsh
 # input parameter
 param(
-    [String]$GreetMsg,
-    [String]$UserName
+    [String]$GreetMsg = "Hello World",
+    [String]$UserName = $env:USERNAME
 )
 
 function GetMsg ($Greeting, $Name) {
-
+    if([string]::IsNullOrWhiteSpace($Name)) { 
+        $Name = $env:HOSTNAME;
+    }
     $ret = "{0} {1}" -f $Greeting, $Name;
     if ($IsWindows) {
         $ret = $ret + ' On Windows';
     }elseif ($IsMacOS) {
         $ret = $ret + ' On macOS';
     }elseif ($IsLinux) {
-        $ret = $ret + ' On Linux';
+        $ret = $ret + ' On Linux' +" $(uname -r)";
     }
     $ret;
 }   
